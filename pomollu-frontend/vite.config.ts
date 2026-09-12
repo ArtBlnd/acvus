e423a5b089549e4717f6386ad300de0fb50b9007
Jun Ryoung Ju <junryoungju@gmail.com>
2026-09-13 00:04:03 +0900

    Remove pomollu ATM

--- pomollu-frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import tailwindcss from '@tailwindcss/vite';
-import wasm from 'vite-plugin-wasm';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [wasm(), tailwindcss(), sveltekit()]
-});
